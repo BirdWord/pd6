082d971e2de4508269ee6dc4dec207e37ef1f609
Student 6 <Student 6@10.8.33.147>
2016-09-26 12:31:49 -0400

    erh

--- Ray/src/chatbox/Raybot.java
+++ Ray/src/chatbox/Raybot.java
@@ -1,6 +1,6 @@
 package chatbox;
 
-import java.util.Scanner;
+import java.util.*;
 public class Raybot {
 	static Topic school;
 	static Scanner scan;
@@ -37,13 +37,33 @@ public class Raybot {
 		while(inLoop){
 			print("Greetings, "+user+". How are you?");
 			response = getInput();
-			if(response.indexOf("good") >= 0)
+			if(findKeyword(response, "good", 0))
 				print("I'm so happy you are good.");
 			else if(response.indexOf("school") > 0){
 				inLoop = false;
 				School.talk();
 			}
 		}
+	}
+	private static boolean findKeyword(String searchString, String key, int startIndex) {
+		String phrase = searchString.trim().toLowerCase();
+		key.toLowerCase();
+		int psn = phrase.indexOf(key);
+		while(psn>=0){
+			String before = " ";
+			String after = " ";
+			if(psn+key.length() < phrase.length()){
+				after = phrase.substring(psn+key.length(), psn+key.length());
+			}
+			if(psn<9){
+				before = phrase.substring(psn-1, psn).toLowerCase();
+			}
+			if(before.compareTo("a")<0 && after.compareTo("a")<0){
+				return true;
+			}
+			psn = phrase.indexOf(key, psn+1);
+		}
+		return false;
 	}
 	public static void promptName(){
 		print("Hi o-o");
