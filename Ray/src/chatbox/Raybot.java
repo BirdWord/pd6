94a5084fa321553368a2fd1034ed6263018112f3
Student 6 <Student 6@10.8.33.147>
2016-09-27 13:17:44 -0400

    complete?

--- Ray/src/chatbox/Raybot.java
+++ Ray/src/chatbox/Raybot.java
@@ -7,6 +7,7 @@ public class Raybot {
 	static String user;
 	static boolean inLoop;
 	static String response;
+	static Topic like;
 	//static int lineCount;
 	public static void main(String[] args) {
 		createTopics();
@@ -18,6 +19,7 @@ public class Raybot {
 	public static void createTopics() {
 		scan = new Scanner(System.in);
 		school = new School();
+		like = new SomethingLike();
 		//lineCount = 0;
 	}
 	public static void print(String x){
@@ -37,15 +39,18 @@ public class Raybot {
 		while(inLoop){
 			print("Greetings, "+user+". How are you?");
 			response = getInput();
-			if(findKeyword(response, "good", 0))
+			if(findKeyword(response, "good", 0)>=0)
 				print("I'm so happy you are good.");
-			else if(response.indexOf("school") > 0){
+			else if(findKeyword(response, "school", 0) > 0){
 				inLoop = false;
 				School.talk();
 			}
+			else if(findKeyword(response, "school", 0) > 0){
+				inLoop = false;
+			}
 		}
 	}
-	private static boolean findKeyword(String searchString, String key, int startIndex) {
+	public static int findKeyword(String searchString, String key, int startIndex) {
 		String phrase = searchString.trim().toLowerCase();
 		key.toLowerCase();
 		int psn = phrase.indexOf(key);
@@ -53,17 +58,34 @@ public class Raybot {
 			String before = " ";
 			String after = " ";
 			if(psn+key.length() < phrase.length()){
-				after = phrase.substring(psn+key.length(), psn+key.length());
+				after = phrase.substring(psn+key.length(), psn+key.length()+1);
 			}
-			if(psn<9){
+			if(psn>0){
 				before = phrase.substring(psn-1, psn).toLowerCase();
 			}
 			if(before.compareTo("a")<0 && after.compareTo("a")<0){
-				return true;
+				if(noNegations(phrase, psn))
+				return psn;
 			}
 			psn = phrase.indexOf(key, psn+1);
 		}
-		return false;
+		return -1;
+	}
+	//helper methods are private.
+	private static boolean noNegations(String phrase, int index){
+		if(index-3 >= 0 && phrase.substring(index-3, index).equals("no ")){
+			return false;
+		}
+		if(index-4 >= 0 && phrase.substring(index-4, index).equals("not ")){
+			return false;
+		}
+		if(index-6 >= 0 && phrase.substring(index-6, index).equals("never ")){
+			return false;
+		}
+		if(index-4 >= 0 && phrase.substring(index-4, index).equals("n't ")){
+			return false;
+		}
+		return true;
 	}
 	public static void promptName(){
 		print("Hi o-o");
---
+++ Ray/src/chatbox/SomethingLike.java
@@ -0,0 +1,29 @@
+package chatbox;
+
+public class SomethingLike implements Topic{
+	private boolean inLikeLoop;
+	private String likeResponse;
+	public void talk(){
+		Raybot.print("What are things you like?");
+		inLikeLoop = true;
+		while(inLikeLoop){
+			likeResponse = Raybot.getInput();
+			int likePsn = Raybot.findKeyword(likeResponse,"like",0);
+			if(likePsn >= 0){
+				String thingsLiked = likeResponse.substring(likePsn+5);
+				Raybot.print("You are such an interesting person because you like things like "+thingsLiked);
+				if(Raybot.findKeyword(thingsLiked, "school", 0)>=0){
+					inLikeLoop = false;
+					Topic.talk();
+				}
+				else{
+					inLikeLoop = false;
+					Raybot.talkForever();
+				}
+			}
+			else
+				Raybot.print("I dont understand you");
+			
+		}
+	}
+}
