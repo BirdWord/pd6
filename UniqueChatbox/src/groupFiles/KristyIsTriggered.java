84f7281099040adb8d5830e3adc92a617675d6e7
Student6 <Student6@10.8.33.166>
2016-09-30 12:22:06 -0400

    triggered

--- UniqueChatbox/src/groupFiles/KristyIsTriggered.java
+++ UniqueChatbox/src/groupFiles/KristyIsTriggered.java
@@ -1,5 +1,18 @@
 package groupFiles;
 
-public class KristyIsTriggered {
+public class KristyIsTriggered implements Topic
+{
+	public boolean isTriggered (String userInput)
+	{
 
+		if (Raybot.findKeyword(userInput, "bye", 0)>=0)
+		{
+			return true;
+		}
+		if (Raybot.findKeyword(userInput, "goodbye", 0)>=0)
+		{
+			return true;
+		}
+		return false;
+	}
 }
