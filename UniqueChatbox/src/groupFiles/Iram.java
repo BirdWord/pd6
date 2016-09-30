977c14faafccde8f8488a57e762ceb24994d8e59
Student 8 <Student 8@10.8.33.155>
2016-09-30 12:27:26 -0400

    IRAMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNM

--- UniqueChatbox/src/groupFiles/Iram.java
+++ UniqueChatbox/src/groupFiles/Iram.java
@@ -1,18 +1,15 @@
 package groupFiles;
 
-//import Raybot.Iram;
-//public class Iram implements Topic{
-//	public boolean isTriggered(String userInput) {
-//		String[] triggers = {}
-//		if(Iram.findKeyword(userInput, "", 0) >= 0){
-//			return true;
-//		}
-//		if(Iram.findKeyword(userInput, "", 0) >= 0){
-//			return true;
-//		}
-//		return false;
-//	}
-//	public static void talk(){
-//
-//	}
+import Raybot.Iram;
+public class Iram implements Topic{
+	public boolean isTriggered(String userInput) {
+		String[] triggers = {}
+		if(Raybot.findKeyword(userInput, "", 0) >= 0){
+			return true;
+		}
+		return false;
+	}
+	public static void talk(){
 
+	}
+}
