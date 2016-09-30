ed43d239998061e136a8a0a0bd21a1560c270544
Student 8 <Student 8@10.8.33.155>
2016-09-30 12:07:33 -0400

    ggsd

--- UniqueChatbox/src/groupFiles/Iram.java
+++ UniqueChatbox/src/groupFiles/Iram.java
@@ -3,11 +3,11 @@ package groupFiles;
 import Raybot.Iram;
 public class Iram implements Topic{
 	public boolean isTriggered(String userInput) {
-		String[] triggers = {"school", "class", "teacher"};
-		if(Iram.findKeyword(userInput, "school", 0) >= 0){
+		String[] triggers = 
+		if(Iram.findKeyword(userInput, "", 0) >= 0){
 			return true;
 		}
-		if(Iram.findKeyword(userInput, "class", 0) >= 0){
+		if(Iram.findKeyword(userInput, "", 0) >= 0){
 			return true;
 		}
 		return false;
