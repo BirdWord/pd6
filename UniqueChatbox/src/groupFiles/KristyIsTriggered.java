b07116032fc1170cdfa9c570b05032970c8bd4e4
Student6 <Student6@10.8.33.166>
2016-09-30 12:29:31 -0400

    fixed

--- UniqueChatbox/src/groupFiles/KristyIsTriggered.java
+++ UniqueChatbox/src/groupFiles/KristyIsTriggered.java
@@ -1,7 +1,49 @@
 package groupFiles;
 
-public class KristyIsTriggered implements Topic
+
+public class KristyIsTriggered implements Topic 
 {
+	private boolean inHelloLoop;
+	private String helloResponse;
+	private int helloCount;
+	private static String[] calmResponses = {"We have already said our hellos. Remember?", "Yes, hello to you too, let's actually talk"};
+	private static String[] angryResponses = {"okay, seriously. This has to stop","You are beginning to annoy me."+"WE'VE SAID HELLO"};
+	
+	public void KristyHello()
+	{
+		helloCount = 0;
+	}
+	public void talk()
+	{
+		inHelloLoop = true;
+		while(inHelloLoop)
+		{
+			helloCount++;
+			printResponse();
+			helloResponse = Raybot.getInput();
+			if (isTriggered(helloResponse))
+			{
+				inHelloLoop = false;
+				Raybot.talkForever();
+			}
+			
+		}
+	}
+	public void printResponse()
+	{
+		int responseIndex = 0;
+		if (helloCount<5)
+		{
+			responseIndex = (int)(Math.random()*calmResponses.length);
+			Raybot.print(calmResponses[responseIndex]);
+			
+		}
+		else
+		{
+			responseIndex = (int)(Math.random()*angryResponses.length);
+			Raybot.print(angryResponses[responseIndex]);
+		}
+	}
 	public boolean isTriggered (String userInput)
 	{
 
