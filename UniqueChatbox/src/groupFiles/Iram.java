69e5ad87bda0a3d4c1d5716af231d342d04e0624
ishahed3914 <ishahed3914@users.noreply.github.com>
2016-10-05 11:52:54 -0400

    Update Iram.java

--- UniqueChatbox/src/groupFiles/Iram.java
+++ UniqueChatbox/src/groupFiles/Iram.java
@@ -18,30 +18,4 @@ public class Iram implements Topic{
 		return false;
 	}
 
-	public void talk(){
-		Iram.print("Think of a number between 1 to 1000");// make the thousand any random number
-		Scanner keyboard = new Scanner(System.in);
-		int a = 1 + (int) (Math.random() * 999);
-		int guess;
-		while((guess != a)){
-			if (guess > a)
-			{  
-				System.out.println("Too high");
-			}
-			else 
-			{
-				System.out.println("Too low");
-			}
-			if (guess >= a-50)
-			{
-				System.out.println("You are getting closer go a little higher");
-			}
-			if(guess <= a+50 )
-			{
-				System.out.println("You are getting closer go a little lower");
-
-			}
-		}
-	}
-
 }
