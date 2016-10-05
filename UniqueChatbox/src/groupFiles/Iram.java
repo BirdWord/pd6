eb5efe04c8dd6bf02f1e820f7f274ba0dce147a3
Iram <Iram@192.168.0.9>
2016-10-04 22:36:13 -0400

    nb

--- UniqueChatbox/src/groupFiles/Iram.java
+++ UniqueChatbox/src/groupFiles/Iram.java
@@ -1,6 +1,15 @@
 package groupFiles;
 
+import Raybot.Iram;
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Iram implements Topic{
+
+	private boolean inLikeLoop;
+	private String likeResponse;
+
 	public boolean isTriggered(String userInput) {
 		String[] triggers = {};
 		if(Raybot.findKeyword(userInput, "", 0) >= 0){
@@ -8,7 +17,31 @@ public class Iram implements Topic{
 		}
 		return false;
 	}
-	public static void talk(){
 
+	public void talk(){
+		Iram.print("Think of a number between 1 to 1000");// make the thousand any random number
+		Scanner keyboard = new Scanner(System.in);
+		int a = 1 + (int) (Math.random() * 999);
+		int guess;
+		while((guess != a)){
+			if (guess > a)
+			{  
+				System.out.println("Too high");
+			}
+			else 
+			{
+				System.out.println("Too low");
+			}
+			if (guess >= a-50)
+			{
+				System.out.println("You are getting closer go a little higher");
+			}
+			if(guess <= a+50 )
+			{
+				System.out.println("You are getting closer go a little lower");
+
+			}
+		}
 	}
+
 }
