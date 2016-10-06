2f8164583d4bd1db1163dfb7db929fa0de97746f
Ray <Ray@192.168.1.107>
2016-10-06 19:01:07 -0400

    hope this goes well

--- UniqueChatbox/src/groupFiles/Raybot.java
+++ UniqueChatbox/src/groupFiles/Raybot.java
@@ -4,62 +4,24 @@ import java.util.*;
 public class Raybot {
 	static Scanner scan;
 	static String user;
-	static boolean inLoop;
 	static String response;
 	static Topic kristy;
 	static Topic ali;
 	static Topic iram;
-	//declare group classes
+	static boolean inLoop;
 	public static void main(String[] args) {
 		createTopics();
 		promptName();
 		talkForever();
 	}
-	public static void createTopics() {
+	private static void createTopics() {
 		scan = new Scanner(System.in);
-		Topic kristy = new KristyIsTriggered();
-		Topic ali = new AliTrigger();
-		Topic iram = new Iram();
+		kristy = new KristyIsTriggered();
+		ali = new AliTrigger();
+		iram = new Iram();
 	}
 	public static void print(String x){
 		System.out.println(x);
-	}
-	public static void promptInput(){
-		print(user+", please input a string.");
-		String userInput = scan.nextLine();
-	}
-	public static void talkForever(){
-		inLoop = true;
-		String[] userSpeech = new String[9001];
-		String[] hateComments = {"Don't test me child.",
-				"I don't think you are using the right hole for the right task.",
-				"It is a pity that you have no shame.",
-				"You are just as pitiful as a grain of sand in the hourglass of eternity.",
-				"Has anything that has ever made sense come out of your mouth?"};
-		while(inLoop){
-			print("Greetings, "+user+". How are you?");
-			int arrayCount = 0;
-			response = getInput();
-			userSpeech[arrayCount] = response;
-			arrayCount++;
-			if(Arrays.asList(userSpeech).contains(response)){
-				print(hateComments[(int)(Math.random()*hateComments.length)]);
-			}
-			else if(findKeyword(response, "good", 0)>=0 || findKeyword(response, "fine", 0)>=0 || findKeyword(response, "great", 0)>=0){
-				print("Great...");
-				print("");
-			}
-			else if(findKeyword(response, "bored", 0)>=0){
-				print("Would you like to play a game?");
-				inLoop = false;
-				kristy.talk();
-			}
-			else if(findKeyword(response, "bored", 0)>=0){
-				print("That is my field of expertise.");
-				inLoop = false;
-				ali.talk();
-			}
-		}
 	}
 	public static int findKeyword(String searchString, String key, int startIndex) {
 		String phrase = searchString.trim().toLowerCase();
@@ -82,7 +44,56 @@ public class Raybot {
 		}
 		return -1;
 	}
-	//helper methods are private.
+	public static void promptName(){
+		print("Hello there, student.");
+		print("I'd advise you tell me your name before you talk to me.");
+		user = scan.nextLine();
+		print("Alright...");
+		print("I'll call you "+user+".");
+	}
+	public static String getInput(){
+		return scan.nextLine();
+	}
+	public static void talkForever(){
+		inLoop = true;
+		String[] userSpeech = new String[100];
+		String[] hateComments = {"Don't test me child.",
+				"I don't think you are using the right hole for the right task.",
+				"It is a pity that you have no shame.",
+				"You are just as pitiful as a grain of sand in the hourglass of eternity.",
+				"Has anything that has ever made sense come out of your mouth?"};
+		while(inLoop){
+			print("Greetings, "+user+". How are you?");
+			int arrayCount = 0;
+			response = getInput();
+			userSpeech[arrayCount] = response;
+			arrayCount++;
+			if(hasItem(userSpeech, response)){
+				print(hateComments[(int)(Math.random()*hateComments.length)]);
+			}
+			else if(keywordFound(response, "good") || keywordFound(response, "fine") || keywordFound(response, "great")){
+				print("Great...");
+				print("Anything you feel like doing?");
+				if(kristy.isTriggered(response)){
+					print("Would you like to play a game?");
+					inLoop = false;
+					kristy.talk();
+				}
+				else if(iram.isTriggered(response)){
+					print("That is my field of expertise.");
+					inLoop = false;
+					iram.talk();
+				}
+				else if(ali.isTriggered(response)){
+					print("Time for a quiz!");
+					inLoop = false;
+					ali.talk();
+				}
+			}
+			
+		}
+	}
+	//helpers
 	private static boolean noNegations(String phrase, int index){
 		if(index-3 >= 0 && phrase.substring(index-3, index).equals("no ")){
 			return false;
@@ -98,32 +109,15 @@ public class Raybot {
 		}
 		return true;
 	}
-	public static void promptName(){
-		print("Hello there, student.");
-		print("I'd advise you tell me your name before you talk to me.");
-		user = scan.nextLine();
-		print("Alright...");
-		print("I'll call you "+user+".");
-	}
-	public static String skipLines(String x, int cutOff){
-		String returnString = "";
-		while(x.length() > 0){
-			String currentLine = "";
-			String nextWord = "";
-			while(currentLine.length() + nextWord.length() <= cutOff && x.length() > 0){
-				currentLine += nextWord;
-				x = x.substring(nextWord.length());
-				int endOfWord = x.indexOf(" ");
-				if(endOfWord == -1)
-					endOfWord = x.length()-1;
-				nextWord= x.substring(0, endOfWord+1);
-			}
-			returnString += currentLine+"\n";
+	private static boolean hasItem(String[] arr, String find){
+		boolean check = false;
+		for(String str: arr){
+			if(str.equals(find))
+				check = true;
 		}
-		return returnString;
+		return check;
 	}
-	public static String getInput(){
-		return scan.nextLine();
+	private static boolean keywordFound(String whole, String keyword){
+		return findKeyword(whole, keyword, 0)>=0;
 	}
-
 }
