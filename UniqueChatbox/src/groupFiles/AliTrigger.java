710101aa5bf2b65b52775fe18c0370089dd4f10c
ali900 <aeldeeb9321@bths.edu>
2016-10-06 22:26:14 -0400

    Ultimate Final Update

--- UniqueChatbox/src/groupFiles/AliTrigger.java
+++ UniqueChatbox/src/groupFiles/AliTrigger.java
@@ -2,14 +2,15 @@ package groupFiles;
 
 public class AliTrigger implements Topic 
 {
-	private static String[] compSciQuestions = {"What is Recursion?","What is Mr.Ip's favorite college?","What is a primitive data type?"};
-	private static String[] answerKey = {"repeat","Berkeley","data that is not in its own class",};
+	private static String[] compSciQuestions = {"What is Recursion?","What is Mr.Ip's favorite college?",
+	"What is a primitive data type?","Is Java your favorite class?"};
+	private static String[] answerKey = {"repeat","Berkeley","data that is not in its own class","yes"};
 	private int answer;
 	private boolean questionCycle;
 	private String userAnswer;
 	public void talk()
 	{
-		Raybot.print("Do you wanna test your abilities in computer science?");
+		Raybot.print("Do you want to test your abilities in a Computer Science Trivia?");
 		while(questionCycle)
 		{
 			
@@ -57,12 +58,25 @@ public class AliTrigger implements Topic
 
 			}
 		}
+		else
+		{
+
+			if (userAnswer == answerKey[3])
+			{
+				Raybot.print("you're correct!");
+			}
+			else
+			{
+				Raybot.print("That can't be the correct answer, try again!!");
+
+			}
+		}
 	}
 //	public String getResponse {
 //		
 //	}
 	public boolean isTriggered(String userInput){
-		String[] triggers = {"testing","question","Ip"};
+		String[] triggers = {"testing","question","test","exam"};
 		for(int i=0; i<triggers.length; i++)
 		{
 			if(Raybot.findKeyword(userInput, triggers[i], 0) >= 0){
