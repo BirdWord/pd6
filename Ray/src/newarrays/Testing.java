aa12c27075d59bc134bd509c3ab1b9e7eff65e2a
Student 6 <Student 6@10.8.33.147>
2016-10-17 13:15:12 -0400

    push

--- Ray/src/newarrays/Testing.java
+++ Ray/src/newarrays/Testing.java
@@ -1,28 +1,71 @@
 package newarrays;
-import java.util.Arrays;
+
 public class Testing {
 	public static void main(String[] args){
-		a();
-		c();
+		printIntegerArray(populate1to50());
+		printIntegerArray(populateRandomIntArray(20));
+		printIntegerArray(generateDiceRollArray(20));
+		printStringArray(generateCardDeck());
+		int[] arr = {0,1,2,3,4};
+		swapItems(0, arr.length-1, arr);
+		printIntegerArray(arr);
 	}
-	private static void a(){
-		int[] a = {1,1,1};
-		System.out.println("array before: "+Arrays.toString(a));
-		b(a);
-		System.out.println("array after: "+Arrays.toString(a));
+	private static void printIntegerArray(int[] arr){
+		System.out.print(arr[0]);
+		for(int i = 1; i<arr.length; i++){
+			System.out.print(", "+arr[i]);
+		}
+		System.out.println();
 	}
-	private static void b(int[] x){
-		x[2] = 6;
+	private static void printStringArray(String[] arr){
+		System.out.print(arr[0]);
+		for(int i = 1; i<arr.length; i++){
+			System.out.print(", "+arr[i]);
+		}
+		System.out.println();
 	}
-	private static void c(){
-		int banana = 0;
-		System.out.println("c method banana before: "+banana);
-		d(banana);
-		System.out.println("c method banana after: "+banana);
+	private static int[] populate1to50(){
+		int[] arr = new int[50];
+		for(int i = 0; i < 50; i++){
+			arr[i] = i+1;
+		}
+		return arr;
 	}
-	private static void d(int banana){
-		System.out.println("d method banana before: " + banana);
-		banana++;
-		System.out.println("d method banana after: " + banana);
+	private static int[] populateRandomIntArray(int length){
+		int[] arr = new int[length];
+		for(int i = 0; i<length; i++){
+			arr[i] = (int)(Math.random()*100);
+		}
+		return arr;
 	}
+	private static int[] generateDiceRollArray(int rolls){
+		int[] rollArray = new int[rolls];
+		for(int i = 0; i<rolls; i++){
+			rollArray[i] = (int)(Math.random()*6)+1+(int)(Math.random()*6)+1;
+		}
+		return rollArray;
+	}
+	private static String[] generateCardDeck(){
+		String[] deck = new String[52];
+		String[] value = {"Ace", "2", "3", "4", "5",  "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+		String[] suit = {"Spades", "Diamonds", "Clubs", "Hearts"};
+		int i = 0;
+		for(String str: value){
+			for(String rts: suit){
+				deck[i] = str+" of "+rts;
+				i++;
+			}
+		}
+		return deck;
+	}
+	private static void swapItems(int index1, int index2, int[] arr){
+		int value = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = value;
+	}
+	private static void shuffleArray(int[] arr){
+		int[] shuf = new int[arr.length];
+		
+	}
+	
 }
