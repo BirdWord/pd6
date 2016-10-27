cb3eb1c2bd9894a6941a2ca89beecf3fd7ae04fa
Student 6 <Student 6@10.8.33.147>
2016-10-27 12:41:42 -0400

    fda

--- Ray/src/homework/HwStuff.java
+++ Ray/src/homework/HwStuff.java
@@ -5,22 +5,11 @@ import java.util.Scanner;
 public class HwStuff {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int[] a = new int[100];
-		int start = 0;
-		for(int i = 0; i<100; i++){
-			start+=(int)(Math.random()*20)+1;
-			a[i] = start;
-		}
-		printIntArray(a);
-		System.out.println("Gimme a working one: ");
-		int lookingFor = Integer.parseInt(scan.nextLine());
-		System.out.println(searchSorted(a,lookingFor));
-		System.out.println("Gimme a broken one: ");
-		int broken = Integer.parseInt(scan.nextLine());
-		System.out.println(searchSorted(a,broken));
+		int[] a = {0,1,2,3,4,5,7,8,9,10};
+		int[] b = {8,9,10};
+		System.out.println(searchSorted(a, 6));
 	}
-	/*public static int searchSorted(int[] sortedArrayToSearch, int key){
+	public static int searchSorted(int[] sortedArrayToSearch, int key){
 		int low = 0;
 		int high = sortedArrayToSearch.length-1;
 		int piv = (low+high)/2;
@@ -35,32 +24,45 @@ public class HwStuff {
 			if(low>high){
 				piv = -1;
 			}
+			System.out.println(piv);
 		}
 		return piv;
-    }*/
-	public static int searchSorted(int[] sortedArrayToSearch, int key){
-		int min = 0;
-		int max = sortedArrayToSearch.length-1;
-		int mid = (min+max)/2;
-		while(min<max){
-			if(key>sortedArrayToSearch[mid]){
-				min = mid+1;
-			}
-			else if(key<sortedArrayToSearch[mid]){
-				max = mid-1;
-			}
-			else{
-				max = mid;
-				min = mid;
-			}
-			mid = (min+max)/2;
-			if(min>max){
-				mid = -1;
-			}
-			System.out.println("min: "+min+" mid: "+mid+" max: "+max);
-		}
-		return mid;
     }
+	public static boolean contains(int[] arr, int[] subarr){
+		for(int i = 0; i<arr.length; i++){
+			if(arr[i] == subarr[0]){
+				for(int j = 1; j<subarr.length; j++){
+					if(i+j+1 < arr.length){
+						if(subarr[j] != arr[i+j])
+							return false;
+					}
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	public static int[] getSubArray(int[] arr, int startIndex, int endIndex){
+		int[] ret = new int[endIndex-startIndex+1];
+		for(int i = 0; i<ret.length; i++){
+			ret[i] = i+startIndex;
+		}
+		return ret;
+	}
+	//count num of elements <d in arr
+	public static int countUnderBound(double[] arr, double d){
+		int count = 0;
+		for(double dub: arr){
+			if(dub<d)
+				count++;
+			else 
+				break;
+		}
+		return count;
+	}
+	public static int calcMedian(double[] arr){
+		return -1;
+	}
 	private static void printIntArray(int[] arr, int startidx, int endidx){
 		System.out.print("[");
 		for(int i = startidx; i<endidx; i++){
