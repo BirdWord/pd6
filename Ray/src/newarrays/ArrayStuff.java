bf729ba8e53d671cafb01af2db16113aaa8d99fb
Student 6 <Student 6@10.8.33.147>
2016-10-28 12:19:24 -0400

    ono

--- Ray/src/newarrays/ArrayStuff.java
+++ Ray/src/newarrays/ArrayStuff.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ArrayStuff {
 
 	public static void main(String[] args) {
-		testPrimes(121);
+		testPrimes(150);
 	}
 	private static void shuffle(int[] arr){
 		for(int i = 0; i<arr.length; i++){
