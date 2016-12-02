40b9f0567db4275b4b2e81984225415474d9487a
Student 6 <Student 6@10.8.33.147>
2016-12-02 13:18:52 -0500

    poosg

---
+++ Ray/src/recursion/Hanoi.java
@@ -0,0 +1,27 @@
+package recursion;
+
+public class Hanoi {
+
+	public static void main(String[] args) {
+		hanoi(2, "A", "B", "C");
+	}
+	public static void hanoi(int discs, String from, String mid, String to){
+		if(discs <= 1){
+			print("Move from "+from+" to "+to+".");
+			moveCount++;
+		}
+		else{
+			//move all but last to B
+			hanoi(discs-1, from, to, mid);
+			//move last to C
+			hanoi(1, from, mid, to);
+			//move all but last one to C
+			hanoi(discs-1, mid, from, to);
+		}
+	}
+	private static int moveCount = 1;
+	private static void print(String string) {
+		System.out.println("Move #"+moveCount+": "+string);
+	}
+
+}
--- Ray/src/recursion/Recursion.java
+++ Ray/src/recursion/Recursion.java
@@ -3,7 +3,34 @@ package recursion;
 public class Recursion {
 
 	public static void main(String[] args) {
-		
+		int numberOfTimes = 5;
+		System.out.println("For Loop:");
+		for(int i = 0; i<numberOfTimes; i++){
+			System.out.println("Hi there.");
+		}
+		Action sayHi = new Action(){
+			public void act(){
+				System.out.println("Hi there.");
+			}
+		};
+		System.out.println("first Recursion:");
+		forLoop1(numberOfTimes, sayHi);
+		System.out.println("second Recursion:");
+		forLoop2(numberOfTimes, sayHi);
 	}
-
+	//standard w/ base case
+	private static void forLoop1(int numberOfTimes, Action action) {
+		if(numberOfTimes <= 1)
+			action.act();
+		else {
+			action.act();
+			forLoop1(numberOfTimes-1, action);
+		}
+	}
+	//test for spd
+	private static void forLoop2(int numberOfTimes, Action action) {
+		action.act();
+		if(numberOfTimes > 1)
+			forLoop2(numberOfTimes-1, action);
+	}
 }
