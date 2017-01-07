4449aa1ca574451749d1575005dc2916bdcb303a
Ray <Ray@192.168.1.107>
2017-01-06 19:25:13 -0500

    fa

--- Ray/src/gui/simon/ButtonInterfaceRay.java
+++
@@ -1,9 +0,0 @@
-package gui.simon;
-
-import java.awt.Color;
-
-import gui.components.Clickable;
-
-public interface ButtonInterfaceRay extends Clickable {
-	void setbuttonColor();
-}
--- Ray/src/gui/simon/ProgressInterfaceRay.java
+++
@@ -1,7 +0,0 @@
-package gui.simon;
-
-import gui.components.Visible;
-
-public interface ProgressInterfaceRay extends Visible {
-	void increaseRound(int i);
-}
