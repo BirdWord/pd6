2e1623d23350434b1dcd920476d40e7ba27e624b
Student 6 <Student 6@10.8.33.171>
2017-05-04 11:56:23 -0400

    adsf

--- Ray/src/timecomplexity/TimeComplexity.java
+++ Ray/src/timecomplexity/TimeComplexity.java
@@ -1,17 +1,7 @@
 package timecomplexity;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class TimeComplexity {
-	private static boolean b;
-	public static void main(String[] args) {
-		String[] wordList = {"dog", "cat", "horse", "rabbit", "bird", "tiger", "unicorn"};
-					String word = "";
-					for(int i = 0; i < wordList.length; i+=2)
-				{
-					  word += wordList[i].substring(0, wordList[i].length()/2);
-				}
-				System.out.print(word);
-
-	}
+	
 }
